920fa2461f7e3363e1b92108011ff282bb0c490b
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-09-22 10:00:00 +0100

    Finished Exercise 2.16 (ComparingIntegers) in Dietel

--- src/DeitelJavaProgrammingBook/ChapterTwo/ComparingIntegers.java
+++ src/DeitelJavaProgrammingBook/ChapterTwo/ComparingIntegers.java
@@ -1,6 +1,9 @@
 package DeitelJavaProgrammingBook.ChapterTwo;
 
 /*
+
+2.16
+
 Write an application that asks the user to enter one integer, obtains
 it from the user and displays whether the number and its square are greater than, equal to, not equal
 to, or less than the number 100. Use the techniques shown in Fig. 2.15.
@@ -9,9 +12,25 @@ to, or less than the number 100. Use the techniques shown in Fig. 2.15.
 import java.util.Scanner;
 
 public class ComparingIntegers {
-
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter one integer: ");
+        int response = userInput.nextInt();
+
+        int square = response * response;
+
+        if (square > 100) {
+            System.out.println(response + " and its square are greater than 100");
+        } else if (square == 100)
+            System.out.println(response + " and its square are equal to 100");
+        else if (square < 100) {
+            System.out.println(response + " and its square are less than 100");
+        } else {
+            System.out.println("Invalid");
+        }
 
     }
 
 }
+
