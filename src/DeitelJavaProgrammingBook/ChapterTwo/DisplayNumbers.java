21d149f000fff0fc90b2b7e01453d4f513d046d6
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-09-26 10:00:00 +0100

    Completed Dietel Exercise 2.14 (Display Numbers)

--- src/DeitelJavaProgrammingBook/ChapterTwo/DisplayNumbers.java
+++ src/DeitelJavaProgrammingBook/ChapterTwo/DisplayNumbers.java
@@ -1,6 +1,8 @@
 package DeitelJavaProgrammingBook.ChapterTwo;
 
 /*
+2.14
+
 Write an application that displays the numbers 1 to 4 on the same line, with each pair of
 adjacent numbers separated by one space. Use the following techniques:
 a) Use one System.out.println statement.
