e2a4d5789756f655ea94dfeb1942f9fe68a85985
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-12-14 10:00:00 +0100

    Completed Dietel Exercise 3.14 (Clock Class)

---
+++ src/DeitelJavaProgrammingBook/ChapterThree/MakingADifference/HeartRates.java
@@ -0,0 +1,95 @@
+package DeitelJavaProgrammingBook.ChapterThree.MakingADifference;
+/*
+ Create a class called HeartRates. The class attributes
+should include the person’s first name, last name and date of birth (consisting of separate attributes
+for the month, day and year of birth). Your class should have a constructor that receives this data as
+parameters. For each attribute provide set and get methods. The class also should include a method
+that calculates and returns the person’s age (in years), a method that calculates and returns the
+person’s maximum heart rate and a method that calculates and returns the person’s target heart rate.
+Write a Java app that prompts for the person’s information, instantiates an object of class HeartRates
+ and prints the information from that object—including the person’s first name, last name and
+date of birth—then calculates and prints the person’s age in (years), maximum heart rate and targetheart-rate range.
+ */
+public class HeartRates {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private int month;
+    private int day;
+    private int yearOfBirth;
+
+    int years;
+    int age = (years * 365) + ((month * 31) + day);
+
+
+    public HeartRates(String firstName, String lastName, String dateOfBirth, int month, int day, int yearOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.month = month;
+        this.day = day;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getMonth() {
+        return month;
+
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+
+    public int calculateUserAgeInYears(int years){
+        int age = (years * 365) + ((month * 31) + day);
+        return age;
+    }
+    public double calculateMinimumHeartRate(double user, double intensity){
+        double heartRateMax = 220 - age;
+        double heartRateMin = heartRateMax * intensity;
+
+        return heartRateMin;
+    }
+}
